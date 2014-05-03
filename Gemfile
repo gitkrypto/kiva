6d64e75c9d6c4d15a11a8d79d892551c15836805
Git Krypto <gitkrypto@gmail.com>
2014-05-03 20:44:08 +0200

    Rspec tests for static page

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
+  gem 'minitest'
   gem 'capybara', '2.1.0'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   resources :transactions
   resources :blocks
 end

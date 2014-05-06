baf36744fcd229358de265c93b45a74358b08a7f
Git Krypto <gitkrypto@gmail.com>
2014-05-06 18:23:38 +0200

    Turn on autoflushing for the logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :debug
+  
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,6 +4,8 @@ require 'nxt'
 scheduler   = Rufus::Scheduler.new
 commander   = NXT::Commander.new
 
+Rails.logger.auto_flushing = true
+
 scheduler.every '5s' do
   commander.poll
 end

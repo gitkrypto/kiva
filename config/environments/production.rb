a0e02358ab577f851ed27b24d28f5efc3603b683
Git Krypto <gitkrypto@gmail.com>
2014-05-07 00:57:01 +0200

    Fix commander it retrieved the wrong number of blocks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  #config.log_level = :debug
+  config.log_level = :info
   
 
   # Prepend all log lines with the following tags.

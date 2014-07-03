f05cea5122b113285d3c27b40e918ea267c39d26
Git Krypto <gitkrypto@gmail.com>
2014-07-03 15:11:44 +0200

    Fix account search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,14 @@ class SearchController < ApplicationController
   def search
     text = params[:search]
       
+    # Is it a Account id
+    thing = Account.where(:native_id => text).first
+    redirect_to account_url(thing) and return if thing
+    
+    # Is it a Account id
+    thing = Account.where(:native_id_rs => text).first
+    redirect_to account_url(thing) and return if thing      
+      
     # Is it a Block height
     thing = Block.where(:height => text.to_i).first
     redirect_to block_url(thing) and return if thing
@@ -22,14 +30,7 @@ class SearchController < ApplicationController
     # Is it a Transaction hash
     thing = nil #Transaction.where(:native_id => text).first
     redirect_to transaction_url(thing) and return if thing
-    
-    # Is it a Account id
-    thing = Account.where(:native_id => text).first
-    redirect_to account_url(thing) and return if thing
-    
-    # Is it a Account id
-    thing = Account.where(:native_id_rs => text).first
-    redirect_to account_url(thing) and return if thing
+   
     
 #    # Is it a Account public key
 #    thing = nil # Account.where(:native_id => text).first
